2a6161f4b5bcef0f7a93dda1ed7ad095c864ac3a
mufidu <mufid.to@gmail.com>
2022-05-30 21:28:45 +0700

    Initial init

---
+++ models/restaurant.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const restaurantSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+    },
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }],
+    picture: {
+        data: Buffer,
+        contentType: String
+    }
+});
+
+module.exports = mongoose.model('Restaurant', restaurantSchema);
